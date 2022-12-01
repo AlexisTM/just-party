da462166fff3ad768a2944a0d45a5d58ab157e3e
Alexis Paques <alexis.paques@gmail.com>
2022-12-01 20:04:05 +0100

    Use hash history to make it work for Github Pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import TestHost from '../views/Test/Host.vue'
 import TestPlayer from '../views/Test/Player.vue'
@@ -8,7 +8,7 @@ import CadavreDrawHost from '../views/CadavreDraw/Host.vue'
 import CadavreDrawPlayer from '../views/CadavreDraw/Player.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
